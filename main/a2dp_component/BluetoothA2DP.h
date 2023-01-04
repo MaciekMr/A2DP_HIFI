64147833f79a1aa9ae89388e368e433d8e2adc22
maciek <macie@DESKTOP-E2ALVLH>
2023-01-04 22:10:05 +0100

    https://github.com/MaciekMr/A2DP_HIFI

---
+++ main/a2dp_component/BluetoothA2DP.h
@@ -0,0 +1,16 @@
+/**
+ * @file BluetoothA2DP.h
+ * @author Phil Schatzmann
+ * @brief Includes all classes
+ * @version 0.1
+ * @date 2022-07-14
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#pragma once
+
+#include "a2dp_component/BluetoothA2DPSource.h"
+#include "a2dp_component/BluetoothA2DPSink.h"
+#include "a2dp_component/BluetoothA2DPSinkQueued.h"
+#include "a2dp_component/SoundData.h"
---
+++ main/main.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <inttypes.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "esp_bt.h"
+#include "esp_bt_main.h"
+#include "esp_bt_device.h"
+#include "esp_gap_bt_api.h"
+#include "esp_a2dp_api.h"
+#include "esp_avrc_api.h"
+#include "BluetoothA2DPSink32.h"
+
+
+extern "C" {
+	void app_main(void);
+}
+
+
+BluetoothA2DPSink32 a2dp_sink; // Subclass of BluetoothA2DPSink
+
+void setup() {
+  a2dp_sink.set_bits_per_sample(32);  
+  a2dp_sink.start("Hifi32bit");  
+}
+
+
+
+
+void app_main(void)
+{
+
+}
